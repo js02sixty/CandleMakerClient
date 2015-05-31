42ecc2bb3d0c8597c879af5a056172f45627a7f1
js02sixty <js02sixty@gmail.com>
2015-05-30 22:34:18 -0400

    more templates

---
+++ src/app/main/about/about.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('cmApp')
+  .controller('AboutCtrl', function ($scope) {})
+;
---
+++ src/app/main/products/products.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('cmApp')
+  .controller('ProductsCtrl', function ($scope) {})
+;
